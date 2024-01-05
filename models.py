7741f35c519e40eec72496efade6d49d1bc2529d
yutian-zhao <yutian_zhao@foxmail.com>
2024-01-06 00:47:54 +0900

    fix bug 1

---
+++ models.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import gymnasium as gym
+
+class FeaturesExtractor(nn.Module):
+    def __init__(self, observation_space: gym.Space, features_dim: int = 128, normalized_image: bool = False, concat=1) -> None:
+        # TODO: normalize
+        super().__init__()
+        # super().__init__(observation_space, features_dim)
+        n_input_channels = observation_space.shape[-1]*concat
+        self.features_dim = features_dim
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 2, (3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(2, 4, (3, 3)),
+            nn.ReLU(),
+            nn.Conv2d(4, 8, (3, 3)),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute shape by doing one forward pass
+        with torch.no_grad():
+            n_flatten = self.cnn(
+                torch.as_tensor(observation_space.sample()).unsqueeze(0).permute(0, 3, 1, 2).repeat(1, concat, 1, 1).float()).shape[1]  # [1,7,7,3]
+
+
+        self.linear = nn.Sequential(
+            nn.Linear(n_flatten, features_dim), nn.ReLU())
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        return self.linear(self.cnn(observations))
+    
+class Head(nn.Module):
+    def __init__(self, input_dim, output_dim, if_prob=False):
+        super().__init__()
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        self.linear = nn.Sequential(
+            nn.Linear(self.input_dim, self.input_dim),
+            nn.ReLU(),
+            nn.Linear(self.input_dim, self.output_dim),
+        )
+        self.if_prob = if_prob
+
+    def forward(self, x, extra_x=None):
+        # TODO: Relu
+        # print("prev_x: ", x.size())
+        if extra_x is not None:
+            # TODO: change to one-hot encoding and may add Norm2d
+            x = torch.cat((x, extra_x), dim=1)
+            # print("extra_x: ", extra_x.size())
+            # print("post_x: ", x.size())
+        x = self.linear(x)
+        if self.if_prob:
+            return nn.functional.softmax(x, dim=1)
+        else:
+            return x
+        
---
+++ utils.py
@@ -0,0 +1,49 @@
+import math
+import random
+from collections import namedtuple, deque
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'skill', 'reward'))
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+    
+
+
+
+# def plot_durations(show_result=False):
+#     plt.figure(1)
+#     durations_t = torch.tensor(episode_durations, dtype=torch.float)
+#     if show_result:
+#         plt.title('Result')
+#     else:
+#         plt.clf()
+#         plt.title('Training...')
+#     plt.xlabel('Episode')
+#     plt.ylabel('Duration')
+#     plt.plot(durations_t.numpy())
+#     # Take 100 episode averages and plot them too
+#     # if len(durations_t) >= 100:
+#     #     means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+#     #     means = torch.cat((torch.zeros(99), means))
+#     #     plt.plot(means.numpy())
+
+#     plt.pause(0.01)  # pause a bit so that plots are updated
+#     if is_ipython:
+#         if not show_result:
+#             display.display(plt.gcf())
+#             display.clear_output(wait=True)
+#         else:
+#             display.display(plt.gcf())
